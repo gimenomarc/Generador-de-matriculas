dbdd04a0f3d85cdc5abc88baaf1add46657891e8
gimenomarc <gimenocervantesmarc@gmail.com>
2023-06-08 20:17:43 +0200

    style effect

--- generador-matriculas/src/Components/LicensePlateGenerator.js
+++ generador-matriculas/src/Components/LicensePlateGenerator.js
@@ -1,4 +1,7 @@
 import React, { useState } from 'react';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import { HiClipboardCopy } from 'react-icons/hi';
+import './LicensePlateGenerator.css'
 
 const LicensePlateGenerator = () => {
   const [licensePlates, setLicensePlates] = useState([]);
@@ -18,50 +21,37 @@ const LicensePlateGenerator = () => {
       plate += randomLetter;
     }
 
-    setLicensePlates([...licensePlates, plate]);
+    setLicensePlates([plate, ...licensePlates]);
   };
 
-  const copyLicensePlate = (licensePlate) => {
-    navigator.clipboard.writeText(licensePlate);
-  };
-
-  const clearLicensePlates = () => {
-    setLicensePlates([]);
+  const copyLicensePlate = (plate) => {
+    navigator.clipboard.writeText(plate);
   };
 
   return (
-    <div className="flex flex-col items-center mt-8">
-      <div className="flex">
-        <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-l mt-4 mr-4"
-          onClick={generateLicensePlate}
-        >
-          Generar Matrícula
-        </button>
-        <button
-          className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-r mt-4"
-          onClick={clearLicensePlates}
-        >
-          Limpiar
-        </button>
-      </div>
+    <div className="flex flex-col items-center">
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4"
+        onClick={generateLicensePlate}
+      >
+        Generar Matrícula
+      </button>
       {licensePlates.length > 0 && (
         <div className="mt-4 w-full">
           <h2 className="text-2xl font-semibold mb-2">Matrículas Generadas</h2>
-          {licensePlates.map((licensePlate, index) => (
-            <div
-              key={index}
-              className="flex items-center bg-gray-100 rounded py-2 px-4 mb-2"
-            >
-              <span className="flex-grow">{licensePlate}</span>
-              <button
-                className="bg-gray-200 hover:bg-gray-300 text-gray-800 font-semibold py-2 px-4 rounded-r"
-                onClick={() => copyLicensePlate(licensePlate)}
-              >
-                Copiar
-              </button>
-            </div>
-          ))}
+          <TransitionGroup component={null}>
+            {licensePlates.map((licensePlate, index) => (
+              <CSSTransition key={index} timeout={500} classNames="slide">
+                <div className="flex items-center bg-gray-200 rounded py-2 px-4 mb-2 license-plate-item">
+                  <span className="flex-grow">{licensePlate}</span>
+                  <HiClipboardCopy
+                    className="cursor-pointer ml-2 copy-icon text-blue-500"
+                    onClick={() => copyLicensePlate(licensePlate)}
+                  />
+                </div>
+              </CSSTransition>
+            ))}
+          </TransitionGroup>
         </div>
       )}
     </div>
