f09002df3be3a6c2dbf724c6228d4edf736718e5
gimenomarc <gimenocervantesmarc@gmail.com>
2023-06-08 19:51:38 +0200

    style

--- generador-matriculas/src/App.js
+++ generador-matriculas/src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
-import LicensePlateGenerator from './Components/LicensePlateGenerator'
+import LicensePlateGenerator from './Components/LicensePlateGenerator';
+import Footer from './Components/Footer';
 
 function App() {
   return (
-    <div className="bg-blue-200 min-h-screen flex flex-col justify-center items-center">
-      <h1 className="text-3xl font-bold text-center my-8 text-gray-800">
-        Generador de matrículas de España
-      </h1>
-      <LicensePlateGenerator />
-      <footer className="mt-8 text-gray-500">
-        <p className="text-sm">
-          Created by{' '}
-          <a
-            href="https://github.com/gimenomarc"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-blue-500"
-          >
-            Marc Gimeno
-          </a>
-        </p>
-      </footer>
+    <div className="bg-blue-200 min-h-screen flex flex-col justify-between items-center pt-16">
+      <div className="flex flex-col justify-center items-center mb-8">
+        <h1 className="text-5xl font-bold text-center my-8 text-gray-800">
+          Generador de matrículas de España
+        </h1>
+        <LicensePlateGenerator />
+      </div>
+      <Footer />
     </div>
   );
 }
--- generador-matriculas/src/Components/LicensePlateGenerator.js
+++ generador-matriculas/src/Components/LicensePlateGenerator.js
@@ -1,11 +1,9 @@
 import React, { useState } from 'react';
 
 const LicensePlateGenerator = () => {
-  const [licensePlate, setLicensePlate] = useState('');
+  const [licensePlates, setLicensePlates] = useState([]);
 
   const generateLicensePlate = () => {
-
-    // Lógica para generar una matrícula aleatoria
     const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     const numbers = '0123456789';
 
@@ -20,32 +18,50 @@ const LicensePlateGenerator = () => {
       plate += randomLetter;
     }
 
-    setLicensePlate(plate);
+    setLicensePlates([...licensePlates, plate]);
   };
 
-  const copyLicensePlate = () => {
+  const copyLicensePlate = (licensePlate) => {
     navigator.clipboard.writeText(licensePlate);
   };
 
+  const clearLicensePlates = () => {
+    setLicensePlates([]);
+  };
+
   return (
-    <div className="flex flex-col items-center">
-      <button
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4"
-        onClick={generateLicensePlate}
-      >
-        Generar Matrícula
-      </button>
-      {licensePlate && (
-        <div className="mt-4 flex items-center">
-          <h2 className="text-2xl font-semibold mr-2">
-            Matrícula Generada: {licensePlate}
-          </h2>
-          <button
-            className="bg-gray-200 hover:bg-gray-300 text-gray-800 font-semibold py-2 px-4 rounded"
-            onClick={copyLicensePlate}
-          >
-            Copiar
-          </button>
+    <div className="flex flex-col items-center mt-8">
+      <div className="flex">
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-l mt-4 mr-4"
+          onClick={generateLicensePlate}
+        >
+          Generar Matrícula
+        </button>
+        <button
+          className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-r mt-4"
+          onClick={clearLicensePlates}
+        >
+          Limpiar
+        </button>
+      </div>
+      {licensePlates.length > 0 && (
+        <div className="mt-4 w-full">
+          <h2 className="text-2xl font-semibold mb-2">Matrículas Generadas</h2>
+          {licensePlates.map((licensePlate, index) => (
+            <div
+              key={index}
+              className="flex items-center bg-gray-100 rounded py-2 px-4 mb-2"
+            >
+              <span className="flex-grow">{licensePlate}</span>
+              <button
+                className="bg-gray-200 hover:bg-gray-300 text-gray-800 font-semibold py-2 px-4 rounded-r"
+                onClick={() => copyLicensePlate(licensePlate)}
+              >
+                Copiar
+              </button>
+            </div>
+          ))}
         </div>
       )}
     </div>
@@ -53,4 +69,3 @@ const LicensePlateGenerator = () => {
 };
 
 export default LicensePlateGenerator;
-
